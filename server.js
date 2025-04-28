09ac7c065ba2fa5093f08bc27fc6b22b1c944f22
NanteRanaivosoa <rnantenainamaminirina@gmail.com>
2025-04-28 18:23:01 +0300

    Add Express app and SQLITe

---
+++ server.js
@@ -0,0 +1,12 @@
+// CRUD App avec Express + SQLite
+
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
